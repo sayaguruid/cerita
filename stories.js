7f8215ab49b50724ec50283bb114e7cb3e86005b
sayaguruid <teacheurs@gmail.com>
2026-03-30 06:12:04 +0700

    Create stories.js

---
+++ stories.js
@@ -0,0 +1,28 @@
+const stories = [
+  {
+    title: "Kelinci vs Kura-kura",
+    pages: [
+      {
+        text: "Pada suatu hari ada kelinci yang sombong",
+        image: "images/1.jpg",
+        audio: "audio/1.mp3"
+      },
+      {
+        text: "Kura-kura menantangnya lomba",
+        image: "images/2.jpg",
+        audio: "audio/2.mp3"
+      }
+    ]
+  },
+
+  {
+    title: "Si Kucing Baik Hati",
+    pages: [
+      {
+        text: "Seekor kucing menemukan anak burung",
+        image: "images/3.jpg",
+        audio: "audio/3.mp3"
+      }
+    ]
+  }
+];
